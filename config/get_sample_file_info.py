56622fe89e4827f19defaa5a5e2d526bc317ddd5
Sophie Walton <sophiewalton@DN0a230453.SUNet>
2023-12-01 16:04:45 -0800

    updated sample name thing

--- config/get_sample_file_info.py
+++ config/get_sample_file_info.py
@@ -1,14 +1,34 @@
 import glob 
 import pandas as pd
 
+def get_R2(x):
+    x_list = x.split('_')
+    x_list[-2] = 'R2'
+    return '_'.join(string_list)
 
+def get_trim1(x):
+    string_sample = x.split('R1')[0][:-1]
+    return string_sample + '-trimmed-pair1.fastq.gz'
+
+def get_trim2(x):
+    string_sample = x.split('R2')[0][:-1]
+    return string_sample + '-trimmed-pair2.fastq.gz'
+
+def get_sampleLane(x):
+    filename = x.split('/')[-1]
+    return filename.split('_R1_001.fastq.gz')[0]
+    
 
 if __name__ == '__main__':
-    sample_folders = glob.glob('/oak/stanford/groups/dpetrov/swalton/data/coalescence-pilot/coalescence_data/*')
-    sample_names = [fname.split('/')[-2] for fname in sample_folders]
-    read1 = [glob.glob(f'{folder}*R1*.gz')[0]for folder in sample_folders]
-    read2 = [glob.glob(f'{folder}*R2*.gz')[0]for folder in sample_folders] 
-    trim1= [f'/scratch/users/swalton/coalescence-pilot-mgx/trimmed/{sample}_L002-trimmed-pair1.fastq.gz' for sample in sample_names]
-    trim2 = [f'/scratch/users/swalton/coalescence-pilot-mgx/trimmed/{sample}_L002-trimmed-pair2.fastq.gz' for sample in sample_names]
-    df = pd.DataFrame(data = {'Sample': sample_names, 'Folder': sample_folders, 'read1': read1 , 'read2': read2, 'trim1': trim1, 'trim2': trim2})
+
+    R1 = glob.glob('/oak/stanford/groups/dpetrov/swalton/data/coalescence-pilot/coalescence_data/*R1*.gz')
+    df = pd.DataFrame(data = {'read1': R1})
+    df['read2'] = df['read1'].transform(get_R2)
+    df['trim1'] = df['read1'].transform(get_trim1)
+    df['trim2'] = df['read2'].transform(get_trim2)
+    df['SampleLane'] = df['read1'].transform(get_sampleLane)
+    df['Sample'] = df['SampleLane'].transform(lambda x: x.split('_')[0])
+
+
+    df = pd.DataFrame(data = {'Sample': sample_names, 'read1': read1 , 'read2': read2, 'trim1': trim1, 'trim2': trim2})
     df.to_csv('sample_fnames.csv')
