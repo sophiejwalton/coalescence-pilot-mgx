00c544853a9b40102af3984aee020647c34adb58
Sophie Walton <sophiewalton@Sophies-MacBook-Pro.local>
2024-11-02 15:29:48 -0700

    scripts

--- workflow/scripts/get_pairwise_fixed_diffs.py
+++ workflow/scripts/get_pairwise_fixed_diffs.py
@@ -34,30 +34,56 @@ def get_pairwise_divergence(sample1_haplotype, sample2_haplotype):
     
 
 
-good_inoculumns = {'AA-mBHI': 'A2-e003Coalescence-mBHI-inoculumn-redo',
- 'AA-mGAM': 'A2-e003Coalescence-Inoculumn-mGAM',
- 'AC/PP-mBHI': 'B3-e003Coalescence-Inoculumn-mBHI',
- 'AC/PP-mGAM': 'B3-e003Coalescence-Inoculumn-mGAM',
- 'AE-mBHI': 'C4-e003Coalescence-mBHI-inoculumn-redo',
- 'AE-mGAM': 'C4-e003Coalescence-Inoculumn-mGAM',
- 'AF-mBHI': 'D5-e003Coalescence-Inoculumn-mBHI',
- 'AF-mGAM': 'D5-e003Coalescence-Inoculumn-mGAM'}
+good_inoculumns = {'AA-AA-mBHI': ['A2-e003Coalescence-mBHI-inoculumn-redo',
+  'A2-e003Coalescence-Inoculumn-mBHI'],
+ 'AA-AA-mGAM': ['A2-e003Coalescence-Inoculumn-mGAM'],
+ 'AA-AC/PP-mBHI': ['A3-e003Coalescence-mBHI-inoculumn-redo'],
+ 'AA-AC/PP-mGAM': ['A3-e003Coalescence-Inoculumn-mGAM'],
+ 'AA-AE-mBHI': ['A4-e003Coalescence-mBHI-inoculumn-redo'],
+ 'AA-AE-mGAM': ['A4-e003Coalescence-Inoculumn-mGAM'],
+ 'AA-AF-mBHI': ['A5-e003Coalescence-mBHI-inoculumn-redo',
+  'A5-e003Coalescence-Inoculumn-mBHI'],
+ 'AA-AF-mGAM': ['A5-e003Coalescence-Inoculumn-mGAM'],
+ 'AC/PP-AA-mBHI': ['B2-e003Coalescence-Inoculumn-mBHI'],
+ 'AC/PP-AA-mGAM': ['B2-e003Coalescence-Inoculumn-mGAM'],
+ 'AC/PP-AC/PP-mBHI': ['B3-e003Coalescence-Inoculumn-mBHI'],
+ 'AC/PP-AC/PP-mGAM': ['B3-e003Coalescence-Inoculumn-mGAM'],
+ 'AC/PP-AE-mBHI': ['B4-e003Coalescence-Inoculumn-mBHI'],
+ 'AC/PP-AE-mGAM': ['B4-e003Coalescence-Inoculumn-mGAM'],
+ 'AC/PP-AF-mBHI': ['B5-e003Coalescence-Inoculumn-mBHI'],
+ 'AC/PP-AF-mGAM': ['B5-e003Coalescence-Inoculumn-mGAM'],
+ 'AE-AA-mBHI': ['C2-e003Coalescence-mBHI-inoculumn-redo'],
+ 'AE-AA-mGAM': ['C2-e003Coalescence-Inoculumn-mGAM'],
+ 'AE-AC/PP-mBHI': ['C3-e003Coalescence-mBHI-inoculumn-redo'],
+ 'AE-AC/PP-mGAM': ['C3-e003Coalescence-Inoculumn-mGAM'],
+ 'AE-AE-mBHI': ['C4-e003Coalescence-mBHI-inoculumn-redo'],
+ 'AE-AE-mGAM': ['C4-e003Coalescence-Inoculumn-mGAM'],
+ 'AE-AF-mBHI': ['C5-e003Coalescence-mBHI-inoculumn-redo'],
+ 'AE-AF-mGAM': ['C5-e003Coalescence-Inoculumn-mGAM'],
+ 'AF-AA-mBHI': ['D2-e003Coalescence-Inoculumn-mBHI'],
+ 'AF-AA-mGAM': ['D2-e003Coalescence-Inoculumn-mGAM'],
+ 'AF-AC/PP-mBHI': ['D3-e003Coalescence-Inoculumn-mBHI'],
+ 'AF-AC/PP-mGAM': ['D3-e003Coalescence-Inoculumn-mGAM'],
+ 'AF-AE-mBHI': ['D4-e003Coalescence-Inoculumn-mBHI'],
+ 'AF-AE-mGAM': ['D4-e003Coalescence-Inoculumn-mGAM'],
+ 'AF-AF-mBHI': ['D5-e003Coalescence-Inoculumn-mBHI'],
+ 'AF-AF-mGAM': ['D5-e003Coalescence-Inoculumn-mGAM']}
 
  
 def get_main(species_dir, save_dir, species, parent_subjects_media):
     parent_subjects_media = parent_subjects_media.split('-')
     media = parent_subjects_media[-1]
-    parent_subjects = parent_subjects_media[:-1]
+    parent_subject = parent_subjects_media[:-1]
     info, depth, freq = load_and_sort_files(species_dir, species)
     med_nonzero_depth = depth.copy().replace(0, np.nan).median(skipna=True)
-    good_samples = med_nonzero_depth[med_nonzero_depth>10.]
+    good_samples = med_nonzero_depth[med_nonzero_depth>5.]
     depth = depth[good_samples.index.values]
     freq = freq[good_samples.index.values]  
     depth_filtered= depth_filtering(depth)
     freq_filtered = freq_masked(freq, depth_filtered)
 
-    inoculumns = [good_inoculumns[f'{parent_subjects[0]}-{media}'], good_inoculumns[f'{parent_subjects[1]}-{media}']]
-    freq_inoculumns = freq_filtered[inoculumns]
+    inoculumns = good_inoculumns[f'{parent_subject[0]}-{media}']
+ #   freq_inoculumns = freq_filtered[inoculumns]
 
    # = get_haplotype(freq_filtered, inoculumns[0])
 
@@ -85,7 +111,9 @@ def get_main(species_dir, save_dir, species, parent_subjects_media):
     ss_df = pd.DataFrame(data = {'s1': s1s, 's2': s2s, 'fixed_diffs': snps_switch, })
     ss_df['Species'] = species
     ss_df['Strain Shift'] = ss_df['fixed_diffs'] > 1000
-    ss_df.to_csv(f'{save_dir}/{species}_fixed_diffs.csv')
+    if '/' in parent_subjects_media:
+        parent_subjects_media = ''.join(parent_subjects_media.split('/'))
+    ss_df.to_csv(f'{save_dir}/{species}_{parent_subjects_media}_fixed_diffs.csv')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='basic filtering of sites')
@@ -97,14 +125,14 @@ if __name__ == '__main__':
                        help = 'location where to get stuff from')
     parser.add_argument('--species', action = 'store', 
                        help = 'species to perform analysis on')
-    parser.add_argument('--parent_subjects', action = 'store', 
+    parser.add_argument('--parent_subject-media', action = 'store', 
                        help = 'parent_subject-media')
     args = parser.parse_args()
     species_dir = f'{args.indir}/{args.species}'
     save_dir = f'{args.outdir}/{args.species}'
     if not path.isdir(save_dir):
         mkdir(save_dir)
-    get_main(species_dir, save_dir, args.species, args.parent_subjects)
+    get_main(species_dir, save_dir, args.species, args.parent_subject-media)
     
 
 
