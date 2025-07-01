723689fe80e89d47472c34a2ac6807931af8841d
Sophie Jean Walton <swalton@sh03-ln08.stanford.edu>
2025-07-01 09:57:14 -0700

    potatoes

---
+++ workflow/scripts/adjust_sp_abundances.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from os import path, mkdir
+from glob import glob
+import argparse
+import itertools as it
+from snp_analysis_tools_sherlock import *
+from evo_changes_tools import *
+from track_snps_funcs import *
+import warnings
+warnings.filterwarnings('ignore')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='basic filtering of sites')
+
+    # add arguments
+   # parser.add_argument('--outdir', action='store',
+     #               help='Outdir prefix where to save stuff')
+   # parser.add_argument('--indir', action = 'store', 
+    #                   help = 'location where to get stuff from')
+  #  args = parser.parse_args()
+
+#    if not path.isdir(args.outdir):
+ #       mkdir(args.outdir) 
+
+
+    species_df = pd.read_csv('workflow/out/midas2_output/species/species_relative_abundance.tsv',delimiter='\t').set_index('species_id')
+    for fname in glob('workflow/out/midas2_output/*/species/species_profile.tsv'):
+        sample = fname.split('/')[-3]
+        sample_df = pd.read_csv(fname,delimiter='\t').set_index('species_id')
+        if sample in species_df.columns.values:
+		species_df.loc[sample_df.index.values, sample] = sample_df['marker_relative_abundance'].values	
+        #print(sample_df)
+       # species_df.loc[sample_df.index.values, sample] = sample_df['marker_relative_abundance'].values
+    species_df.to_csv('workflow/out/midas2_output/species/species/species_relative_abundance_alt.tsv',sep='\t')
+
