ade56d9fc8eb8765acd3d2d556cc0ff5851ed532
Sophie Jean Walton <swalton@sh03-ln06.stanford.edu>
2026-08-20 21:06:42 -0700

    sjw

---
+++ workflow/scripts/get_no_reads.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import gzip 
+import numpy as np 
+
+import gzip
+
+# google answer
+def num_reads(filename):
+    count = 0
+    with gzip.open(filename, 'rt') as f:
+        for i, _ in enumerate(f):
+            pass
+    return (i + 1) // 4
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('TableS1_only1.csv')
+
+    df['fname2'] = df['fname'].transform(lambda x: x.split('.1.fastq.gz')[0]+ '.2.fastq.gz')
+    df['accession'] = 'TBA'
+    df['num_reads1'] = np.nan
+   # df['num_reads2'] = np.nan
+    df = df.set_index('fname')
+    for i, fname in enumerate(df.index.values):
+        print(i, df.loc[fname,'fname2'])
+        df.loc[fname,'num_reads1'] = num_reads(fname)
+       # df.loc[fname,'num_reads2'] = num_reads(df.loc[fname,'fname2'])
+       # print(df.loc[fname,:])
+  #  df['total_reads'] = df['num_reads1'] + df['num_reads2'] 
+    df.to_csv('TableS1_with_num_reads_only1.csv')
+        
+  
